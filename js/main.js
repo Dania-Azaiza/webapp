a9f2453fae61706c42f39096b13fe5999c5b578d
Dania Azaiza <dania.azaiza@gmail.com>
2016-03-20 12:52:43 +0200

    Added first implementation of main

--- js/main.js
+++ js/main.js
@@ -1,25 +1,22 @@
-(function(){
-	var tab=document.getElementsByClassName("tabs").firstChild;
+(function(){ 
+	alert("Hello");
 
-	var tabs=document.getElementsByTagName("a");
-	var tabClick=function(){
-		alert("dania");	
-	};
-	for(var i=0,len=tabs.length;i<len;i=i+1){
-		UTILS.addEvent(tabs[i],"click",tabClick);
+	
+	var tabslist = document.getElementById("tabs-list").getElementsByTagName("a");
+	
+	for(i = 0;i < tabslist.length; i++)
+	{
+		var onTabClick = function(){
+			alert("dan");	
+		};
+		alert(tabslist[i].innerHTML);
+		UTILS.addEvent(tabslist[i],"click",onTabClick(tabslist[i].innerHTML));
+	}	
+	
 
-	}
+	//UTILS.addEvent(tab,"click",onTabClick);
+	
+	// for(var i=0,len=tabs.length;i<len;i=i+1){
+		// UTILS.addEvent(tabs[i],"click",tabClick);
+	// }
 }());
-/*
-$(document).ready(function(){ 
-});
-
-
-
-
-
-
-$(".tabs a").click(function(){
-   	
-   	alert("haha1");
- });*/

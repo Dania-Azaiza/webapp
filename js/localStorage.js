5124d91b28193a5b8606ac2b75b509e5586513ce
Dania Azaiza <dania.azaiza@gmail.com>
2016-03-20 16:04:25 +0200

    Add local storage JS

---
+++ js/localStorage.js
@@ -0,0 +1,43 @@
+var LOCALSTORAGE = 
+(function () {
+
+	return {
+			SaveState: function(selectedTab, QuickReportsState, MyTeamFolderState){
+				var fullState = JSON.parse(localStorage.getItem('settings'));
+				
+				if (selectedTab === undefined || QuickReportsState === undefined || MyTeamFolderState === undefined)
+					throw Error('All parameters must be defined');
+				
+				fullState.selectedTab = selectedTab;
+				fullState.QuickReportsState = QuickReportsState;
+				fullState.MyTeamFolderState = MyTeamFolderState;
+				
+				localStorage.setItem('settings', JSON.stringify(fullState));
+			},
+			
+			
+			GetLastTab : function(){
+				var fullState = JSON.parse(localStorage.getItem('settings'));
+				if (!fullState) 
+					return null;
+				
+				return fullState.selectedTab;
+			},
+			
+			GetQuickReportsState: function(){
+				var fullState = JSON.parse(localStorage.getItem('settings'));
+				if (!fullState) 
+					return null;
+				
+				return fullState.QuickReportsState;
+			},
+			
+			GetMyTeamFolder: function(){
+				var fullState = JSON.parse(localStorage.getItem('settings'));
+				if (!fullState) 
+					return null;
+				
+				return fullState.MyTeamFolderState;
+			}
+		};
+}());

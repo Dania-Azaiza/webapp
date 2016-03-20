860cefffcaffc4fdcd3eac90757156fe29a0cf40
Dania Azaiza <dania.azaiza@gmail.com>
2016-03-20 13:34:26 +0200

    Add ajax call for notifications

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
 (function(){ 
 	alert("Hello");
-
 	
-	var tabslist = document.getElementById("tabs-list").getElementsByTagName("a");
+	// var tabslist = document.getElementById("tabs-list").getElementsByTagName("a");
 	
-	for(i = 0;i < tabslist.length; i++)
-	{
-		var onTabClick = function(){
-			alert("dan");	
-		};
-		alert(tabslist[i].innerHTML);
-		UTILS.addEvent(tabslist[i],"click",onTabClick(tabslist[i].innerHTML));
-	}	
+	// for(i = 0;i < tabslist.length; i++)
+	// {
+		// var onTabClick = function(){
+			// alert("dan");	
+		// };
+		// alert(tabslist[i].innerHTML);
+		// UTILS.addEvent(tabslist[i],"click",onTabClick(tabslist[i].innerHTML));
+	// }	
+	
+	
+	// =============== Stage 3 ===============
+	UTILS.ajax('./data/config.json', { 
+		type: 'json',
+		method: 'GET',
+		done: function (response) {
+			var notificationsDiv = document.getElementById('notifications');
+			if (!response.notification) {
+				notificationsDiv.textContent = '';
+				notificationsDiv.classList.add('hidden');
+			}
+			else {
+				notificationsDiv.textContent = response.notification;
+				notificationsDiv.classList.remove('hidden');
+			}
+		}
+	});	
 	
 
 	//UTILS.addEvent(tab,"click",onTabClick);
--- js/plugins.js
+++ js/plugins.js
@@ -5,6 +5,8 @@
 var UTILS = (function () {
 
 	return {
+		
+		
 		/**
 		 * Check if a given value is a plain Object
 		 *
@@ -15,6 +17,8 @@ var UTILS = (function () {
 			var toString = Object.prototype.toString;
 			return (toString.call(o) === toString.call({}));
 		},
+		
+		
 		addEvent:function(elem,type,handler){
 			if(typeof addEventListener!=="undefined"){
 				elem.addEventListener(type,handler,false);
@@ -26,7 +30,9 @@ var UTILS = (function () {
 				elem["on"+type]=handler;
 			}
 		},
-		 removeEvent:function(elem,type,handler){
+		
+		
+		removeEvent:function(elem,type,handler){
 			if(typeof removeEventListener!=="undefined"){
 				elem.removeEventListener(type,handler,false);
 			}
@@ -47,6 +53,80 @@ var UTILS = (function () {
 		},*/
 
 
+	},
+	
+	/**
+	 * AJAX helper function (similar to jQuery, but far from it!)
+	 *
+	 * @param {string} url     URL for the ajax request
+	 * @param {Object} options AJAX settings
+	 */
+	ajax: function (url, options) {
+		var xhr = new XMLHttpRequest(),
+			method = 'GET',
+			options = UTILS.isObject(options) ? options : {};
+
+		// Check if "method" was supplied
+		if (options.method) {
+			method = options.method;
+		}
+
+		// Setup the request
+		xhr.open(method.toUpperCase(), url);
+
+		xhr.onreadystatechange = function () {
+			var status;
+
+			// If request finished
+			if (xhr.readyState === 4) {
+				status = xhr.status;
+
+				// If response is OK or fetched from cache
+				if ((status >= 200 && status < 300) || status === 304) {
+					var res = xhr.responseText,
+						contentType = xhr.getResponseHeader('Content-Type');
+
+					// If server sent a content type header, handle formats
+					if (contentType) {
+						// Handle JSON format
+						if (contentType === 'text/json' ||
+							contentType === 'application/json') {
+
+							// JSON throws an exception on invalid JSON
+							try {
+								res = JSON.parse(res);
+							} catch (err) {
+								// Trigger "fail" callback if set
+								if (options.fail) {
+									options.fail.call(xhr, err);
+									return;
+								}
+							}
+						// Handle XML format
+						} else if (contentType === 'text/xml' ||
+							contentType === 'application/xml') {
+							// responseXML returns a document object
+							res = xhr.responseXML;
+
+							// if XML was invalid, trigger fail callback
+							if (res === null && options.fail) {
+								options.fail.call(xhr, 'Bad XML file');
+								return;
+							}
+						}
+					}
+
+					// Trigger done callback with the proper response
+					if (options.done) {
+						options.done.call(xhr, res);
+					}
+				}
+
+			}
+		};
+
+		// Fire the request
+		xhr.send(null);
 	};
 }());
 
